85ced75481bbe679f0fbf91cd121350a074c599f
Nishchayg7 <nishchayg7@gmail.com>
2017-11-11 01:42:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/application/shopinterio/AccountActivity/MainScreen.java
@@ -0,0 +1,15 @@
+package com.example.application.shopinterio.AccountActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.application.shopinterio.R;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
